fa9a93d91a8ae9e457f40635f1e42b55c782adbd
Kevin04C <kevincespedes2004@gmai.com>
2022-11-30 21:19:40 -0500

    add show photo user

--- src/ecommer/components/NavBar.jsx
+++ src/ecommer/components/NavBar.jsx
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { Options } from "./Options";
 
 export const NavBar = () => {
-  const dispatch = useDispatch();
+  
+  const { secure_url, nombre } = useSelector((state) => state.auth);
   const navigate = useNavigate();
   const { status } = useSelector((state) => state.auth);
   const [showOptions, setShowOptions] = useState(false);
-  const { nombre } = useSelector((state) => state.auth);
-
+  
 
   const handleChangeOptions = () => {
     setShowOptions(!showOptions);
@@ -22,28 +22,38 @@ export const NavBar = () => {
   return (
     <>
       <nav className="bg-white border border-b shadow-sm py-4 px-3 sticky left-0 top-0 z-20">
-        <div className="md:max-w-7xl mx-auto md:flex-row flex flex-col justify-between items-center relative">
+        <div className="md:max-w-7xl mx-auto md:flex-row flex flex-col justify-between items-center relative p-2">
           <h2
             className="text-xl md:text-2xl font-bold cursor-pointer mb-3 md:mb-0"
             onClick={handleClickHome}
           >
-            <span className="text-transparent bg-clip-text bg-gradient-to-tr from-red-900 to-red-500"> <i className="fa-solid fa-martini-glass"></i> LICOR</span> STORE
+            <span className="text-transparent bg-clip-text bg-gradient-to-tr from-red-900 to-red-500">
+              {" "}
+              <i className="fa-solid fa-martini-glass"></i> LICOR
+            </span>{" "}
+            STORE
           </h2>
           <div className="flex gap-5 md:gap-8 ">
             {status === "authenticated" ? (
               <>
                 <p
-                  className="text-gray-600 md:text-lg hover:text-black cursor-pointer transition-colors"
+                  className="text-gray-600 md:text-lg hover:text-black cursor-pointer transition-colors flex items-center"
                   onClick={handleClickCart}
                 >
                   <i className="fa-solid fa-cart-shopping mr-3"></i>
                   Mi carrito
                 </p>
                 <p
-                  className="text-gray-600 md:text-lg hover:text-black cursor-pointer transition-colors "
+                  className="text-gray-600 md:text-lg hover:text-black cursor-pointer transition-colors flex items-center gap-2"
                   onClick={handleChangeOptions}
                 >
-                  <i className="fa-solid fa-user mr-2"></i>
+                  <figure className="w-7">
+                    <img
+                      className="w-full rounded-full"
+                      src={secure_url}
+                      alt={nombre}
+                    />
+                  </figure>
                   {nombre}
                 </p>
                 {showOptions && <Options />}
